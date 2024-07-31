cab7af575988b7b13002c912281a85b5d50caff3
Kenny <kennycatches@Gmail.com>
2024-08-01 10:32:19 +1200

    tab name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,6 +14,7 @@ export default function Header() {
     return (
         <div>
             <Typography className={style.heading} variant="h2">Hey! I'm Ken.</Typography>   
+            <Typography sx={{top: 120}} className={style.heading} variant="subtitle1">(This is a work in progress)</Typography>
             <Box sx={{width: 900, borderTop: 1, borderBottom: 1, borderColor: "lightBlue"}} className={style.box}>
                 <Button sx={{width: 200}} className={style.homeButton} onClick={handleNavigation('/')}>HOME</Button>
                 <Button sx={{width: 200}} className={style.projectsButton} onClick={handleNavigation('/projects')}>PROJECTS</Button>
